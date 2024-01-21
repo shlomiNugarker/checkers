451230c8899a2e6beeb2a702483f92ba0466c3f8
Shlomi Nugarker <shlomin1231@gmail.com>
2024-01-21 08:38:25 +0200

    Header

---
+++ src/cmps/HeaderCmp.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>Header</header>
+</template>
+
+<script lang="ts">
+export default {
+  props: [],
+  name: 'HeaderCmp',
+
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
