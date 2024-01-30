0b5c1943fa7e2296a248c4ea9bd556fa42fc5021
Shlomi Nugarker <shlomin1231@gmail.com>
2024-01-30 16:09:55 +0200

    comment

--- src/checkers/Board.ts
+++ src/checkers/Board.ts
@@ -13,7 +13,7 @@ export class Board {
       for (let j = 0; j < 8; j++) {
         let piece = null
         const coord = { i, j }
-        // BLACK
+        // Initialize black pieces
         if (i === 0 && j % 2 === 0) {
           piece = new Piece({ name: PieceType.Black, game, coord })
         } else if (i === 1 && j % 2 !== 0) {
@@ -21,7 +21,7 @@ export class Board {
         } else if (i === 2 && j % 2 === 0) {
           piece = new Piece({ name: PieceType.Black, game, coord })
         }
-        // WHITE
+        // Initialize white pieces
         else if (i === 5 && j % 2 !== 0) {
           piece = new Piece({ name: PieceType.White, game, coord })
         } else if (i === 6 && j % 2 === 0) {
