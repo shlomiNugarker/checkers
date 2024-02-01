700b26911e67894674f21e2232a4fb1347434409
Shlomi Nugarker <shlomin1231@gmail.com>
2024-02-01 20:52:47 +0200

    ControllerCmp

---
+++ src/cmps/ControllerCmp.vue
@@ -0,0 +1,21 @@
+<template>
+  <aside>ControllerCmp</aside>
+</template>
+
+<script lang="ts">
+export default {
+  props: [],
+  name: 'ControllerCmp',
+
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+aside {
+  width: 30%;
+  background-color: #262522;
+}
+</style>
