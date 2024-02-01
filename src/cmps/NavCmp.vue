92fc042daea08ccda00fd6eb15e6e8fe274265f9
Shlomi Nugarker <shlomin1231@gmail.com>
2024-02-01 20:53:04 +0200

    NavCmp

---
+++ src/cmps/NavCmp.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav>
+    <div class="logo">logo</div>
+    <ul>
+      <li>Play</li>
+      <li>Puzzles</li>
+      <li>Learn</li>
+    </ul>
+    <div>
+      <input type="text" placeholder="Search" />
+      <button type="button">Sign Up</button>
+      <button type="button">Log In</button>
+    </div>
+    <div>
+      <button color="dark">
+        <span>x</span>
+        <p>English</p>
+      </button>
+      <button color="dark">
+        <span>x</span>
+        <p>Light UI</p>
+      </button>
+      <button color="dark">
+        <span>x</span>
+        <p>Help</p>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  props: [],
+  name: 'NavCmp',
+
+  data() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  width: 20%;
+  background-color: #262522;
+}
+</style>
