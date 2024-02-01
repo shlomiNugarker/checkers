4029b3c37c52cf988676b6b289dae005c140587c
Shlomi Nugarker <shlomin1231@gmail.com>
2024-02-01 20:53:18 +0200

    UserPreviewCmp

---
+++ src/cmps/UserPreviewCmp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>UserPreviewCmp</div>
+</template>
+
+<script lang="ts">
+export default {
+  props: [],
+  name: 'UserPreviewCmp',
+
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  background-color: #262522;
+}
+</style>
