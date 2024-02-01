3f8bea9a0e63da250090cd335c42d1a4420b85cf
Shlomi Nugarker <shlomin1231@gmail.com>
2024-02-01 23:58:10 +0200

    delete about cmp

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'HomeView',
       component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'AboutView',
-      component: AboutView
     }
   ]
 })
